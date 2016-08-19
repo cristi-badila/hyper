082625bea91fc348b304ee8a165f175b16c34ee8
cristibadila <cristi.badila@gmail.com>
2016-08-19 12:30:01 +0300

    Add missing specs for CallDescriptor

---
+++ Tests.HyperMock/Core/CallDescriptorTests.cs
@@ -0,0 +1,17 @@
+﻿namespace HyperMock.Universal.Tests.Core
+{
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using Universal.Core;
+
+    [TestClass]
+    public class CallDescriptorTests
+    {
+        [TestMethod]
+        public void Constructor_ByDefault_InitializesParameterMatchers()
+        {
+            var callDescriptor = new CallDescriptor();
+
+            Assert.IsNotNull(callDescriptor.ParameterMatchers);
+        }
+    }
+}
