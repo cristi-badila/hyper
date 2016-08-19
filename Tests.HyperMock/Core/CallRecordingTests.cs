506a7624c73ae65960e2d163d0b36ec9026437b4
cristibadila <cristi.badila@gmail.com>
2016-08-19 12:34:51 +0300

    Add missing specs for CallRecording

---
+++ Tests.HyperMock/Core/CallRecordingTests.cs
@@ -0,0 +1,20 @@
+﻿namespace HyperMock.Universal.Tests.Core
+{
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using Universal.Core;
+
+    [TestClass]
+    public class CallRecordingTests
+    {
+        [TestMethod]
+        public void Constructor_Always_SetsGivenValuesOnTheCreatedInstance()
+        {
+            var callArguments = new object[0];
+
+            var callRecording = new CallRecording("testName", callArguments);
+
+            Assert.AreEqual("testName", callRecording.MemberName);
+            Assert.AreSame(callArguments, callRecording.Arguments);
+        }
+    }
+}
